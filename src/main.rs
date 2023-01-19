2feb67a48f9c2bc4bdfc0e81cf04b2886e82de39
Jared Chesebro <jaredchesebro@novuslabs.com>
2023-01-18 16:16:55 -0800

    Functional program, not solo, need to go through and write notes

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,65 @@
 use std::collections::HashMap;
 use std::io;
 
-fn main() {
-    let mut departments = HashMap::new();
+use std::io::BufRead;
 
-    departments.insert("Engineering".to_string(),
-    "Tom Riddle".to_string(),
-    );
+fn main() {
+    let mut employees: HashMap<String, Vec<String>> = HashMap::new();
+    let stdin = io::stdin();
+    println!("Type 'Add <name> to <department>' to add an employee");
+    println!("Type 'List <department>' to list the employees of a department");
+    println!("Type 'All' to list all employees by department");
+    println!("Type 'Quit' to quit");
+
+    for line in stdin.lock().lines() {
+        let input = line.expect("error: unable to read user input");
+        match Command::from_input(&input) {
+            Some(Command::Add { dept, name }) => employees.entry(dept).or_default().push(name),
+            Some(Command::List(dept)) => match employees.get(&dept) {
+                Some(names) => {
+                    for name in names {
+                        println!("{}: {}", dept, name);
+                    }
+                }
+                None => println!("I don't recognize that departmnet!"),
+            },
+            Some(Command::All) => {
+                for (dept, names) in &employees {
+                    let mut names = names.clone();
+                    names.sort();
+                    for name in names {
+                        println!("{}: {}", dept, name);
+                    }
+                }
+            }
+            Some(Command::Quit) => break,
+            None => println!("Input error!"),
+        }
+    }
 }
 
-// write a function that allows user to select whether to add employees / retreive
-// employees from database. If select to add, choose which department and then their
-// name, from there push the name to the department that *matches*
-// when retreiving employees from a department sort them alphabetically
+enum Command {
+    // Using named fields instead of Add(String, String) because dept and name
+    // are the same type and could get mixed up.
+    Add { dept: String, name: String },
+    List(String),
+    All,
+    Quit,
+}
 
-// Using a hash map and vectors, create a text interface to allow a user to add
-// employee names to a department in a company, ex:
-// "Add Sally to Engineering", or "Add Amir to Sales".
-// Then let the user retrieve a list of all people in a department or all people
-// in the company by department sorted alphabetically.
+impl Command {
+    fn from_input(s: &str) -> Option<Self> {
+        let words: Vec<&str> = s.trim().split_whitespace().collect();
+        // "Slice destructuring / slice pattern matching" for more info
+        match words.as_slice() {
+            ["All"] => Some(Command::All),
+            ["Quit"] => Some(Command::Quit),
+            ["List", dept] => Some(Command::List(dept.to_string())),
+            ["Add", name, "to", dept] => Some(Command::Add {
+                dept: dept.to_string(),
+                name: name.to_string(),
+            }),
+            _ => None,
+        }
+    }
+}
